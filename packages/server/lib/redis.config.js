933e099fa6a0d12d729cbeda78dcb68454732c1d
codebysid <codebysidd@gmail.com>
2024-11-11 17:58:48 +0530

    redis on error added

--- packages/server/lib/redis.config.js
+++ packages/server/lib/redis.config.js
@@ -2,5 +2,7 @@ require("dotenv").config();
 const Redis = require("redis");
 
 const redisClient = Redis.createClient(process.env.REDIS_URL);
-
+redisClient.on("error", (err) => {
+  console.log("Redis error occurred", err);
+});
 module.exports = { redisClient };
