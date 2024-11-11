eedf4f1cb0576130d8770974c1f08e0e3b073717
codebysid <codebysidd@gmail.com>
2024-11-11 14:45:39 +0530

    .tsbuildingo added to gitignore

--- packages/client/src/components/CustomError.tsx
+++ packages/client/src/components/CustomError.tsx
@@ -9,14 +9,16 @@ export default function CustomError() {
   return (
     <div className=" flex flex-col items-center gap-4 text-center">
       <span className="lg:text-xl">
-        {error instanceof Error
-          ? error.message
-          : (
-              <div className=" flex flex-col gap-2">
-                <span>{(error as any)?.statusText}</span>
-                <span>{(error as any)?.data}</span>
-              </div>
-            ) || "An unknown error occurred."}
+        {error instanceof Error ? (
+          error.message
+        ) : error ? (
+          <div className="flex flex-col gap-2">
+            <span>{(error as any)?.statusText || "Status unknown"}</span>
+            <span>{(error as any)?.data || "No additional data"}</span>
+          </div>
+        ) : (
+          "An unknown error occurred."
+        )}
       </span>
       <HomeButton route="/">Create New Playlist</HomeButton>
     </div>
